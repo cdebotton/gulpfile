34a4da825eacb81e8ba5e6c436b37dbc6130b231
Christian de Botton <cdebotton@gmail.com>
2014-08-15 09:29:16 -0400

    Not Found Handler.

---
+++ src/components/NotFoundHandler.jsx
@@ -0,0 +1,17 @@
+/** @jsx React.DOM */
+
+'use strict';
+
+var React = require('react');
+
+var AboutHandler = React.createClass({
+  render: function() {
+    return (
+      <div className="AboutHandler">
+        <h1>Not Found, 404</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = AboutHandler;
--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -2,12 +2,13 @@
 
 'use strict';
 
-var Routes        = require('react-router').Routes;
-var Router        = require('react-router').Router;
-var Route         = require('react-router').Route;
-var App           = require('./App.jsx');
-var IndexHandler  = require('./IndexHandler.jsx');
-var AboutHandler  = require('./AboutHandler.jsx');
+var Routes          = require('react-router').Routes;
+var Router          = require('react-router').Router;
+var Route           = require('react-router').Route;
+var App             = require('./App.jsx');
+var IndexHandler    = require('./IndexHandler.jsx');
+var AboutHandler    = require('./AboutHandler.jsx');
+var NotFoundHandler = require('./NotFoundHandler.jsx');
 
 module.exports = (
   <Routes location="history">
@@ -15,5 +16,6 @@ module.exports = (
       <Route name="/" handler={IndexHandler} />
       <Route name="about" handler={AboutHandler} />
     </Route>
+    <Route path="*" handler={NotFoundHandler} />
   </Routes>
 );
