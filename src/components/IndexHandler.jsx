5315af3e0930c03746a4e4da99b4d8a001559e47
Christian de Botton <cdebotton@gmail.com>
2014-08-15 09:23:30 -0400

    Fixed indexhandler.

---
+++ src/components/IndexHandler.jsx
@@ -0,0 +1,17 @@
+/** @jsx React.DOM */
+
+'use strict';
+
+var React = require('react');
+
+var IndexHandler = React.createClass({
+  render: function() {
+    return (
+      <div className="IndexHandler">
+        <h2>Index</h2>
+      </div>
+    );
+  }
+});
+
+module.exports = IndexHandler;
--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -6,11 +6,13 @@ var Routes        = require('react-router').Routes;
 var Router        = require('react-router').Router;
 var Route         = require('react-router').Route;
 var App           = require('./App.jsx');
+var IndexHandler  = require('./IndexHandler.jsx');
 var AboutHandler  = require('./AboutHandler.jsx');
 
 module.exports = (
   <Routes location="history">
     <Route handler={App}>
+      <Route name="/" handler={IndexHandler} />
       <Route name="about" handler={AboutHandler} />
     </Route>
   </Routes>
