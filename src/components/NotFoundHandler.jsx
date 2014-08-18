b8cb0c04c714967440f6abf0fc01a1beb5a82982
Christian de Botton <cdebotton@gmail.com>
2014-08-18 06:58:35 -0400

    Styling hello world.

--- src/components/NotFoundHandler.jsx
+++ src/components/NotFoundHandler.jsx
@@ -3,12 +3,14 @@
 'use strict';
 
 var React = require('react');
+var Link  = require('react-router').Link;
 
 var AboutHandler = React.createClass({
   render: function() {
     return (
       <div className="AboutHandler">
         <h1>Not Found, 404</h1>
+        <p>Go <Link to="/">home</Link>.</p>
       </div>
     );
   }
